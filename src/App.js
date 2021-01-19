29ef9ef82a61216aabfab475fafd57d17ef96442
Akhror <akhsoliev@gmail.com>
2021-01-19 10:17:33 +0500

    search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+const api = {
+  key: '3d17197cb08ab8c23ac139348e444742',
+  base: 'https://openweathermap.org/data/2.5/',
 }
 
-export default App;
+function App() {
+  return <div className="app">
+    <main>
+      <div className="search-box">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+        />
+      </div>
+    </main>
+  </div>
+}
+
+export default App
